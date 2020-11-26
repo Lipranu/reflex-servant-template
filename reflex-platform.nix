b6000f950723cbe4b1da2bb5a46da58b6845d62a
Igor Belousov <lipranu@gmail.com>
2020-11-27 03:46:43 +0700

    Update reflex-platform to develop branch

--- reflex-platform.nix
+++ reflex-platform.nix
@@ -1,10 +1,9 @@
-# Reflex Platform release 0.6.1.0
 { pkgs ? import <nixpkgs> {} }:
 let
   reflex-platform = pkgs.fetchFromGitHub {
     owner = "reflex-frp";
     repo = "reflex-platform";
-    rev = "673c2622b288504bb57006e6b440d5240ce1865c";
-    sha256  = "0ny8dah1s3amvs1h5k8j1zwzmh07a12af833b6g32cxf0qlj3v0l";
+    rev = "097e0ef01539f1ba23bd6a4e2aaedc7d2b114a26";
+    sha256  = "0998wvqb8fv1ng5mbs0d7iivvgkbc6f5wahb6nhf42m79vcgbw2i";
   };
 in import reflex-platform {}
