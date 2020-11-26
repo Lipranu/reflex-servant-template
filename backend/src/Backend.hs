6195c15a9e1de0e46b09104be479a3cdace47b0b
Igor Belousov <lipranu@gmail.com>
2020-11-26 21:29:46 +0700

    Update backend module

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators    #-}
 
-module Backend (backend) where
+module Backend ( backend ) where
 
 import Common
-import Data.Proxy ( Proxy )
+import Data.Proxy  ( Proxy )
 import Network.Wai ( Application )
-import Servant --(serve, Proxy(..), Server, JSON, Get, (:>))
+import Servant     ( Server, Raw, Proxy (..), (:<|>) (..)
+                   , serve, serveDirectoryFileServer
+                   )
 
-type API = Raw
+type Backend = API :<|> Raw
 
 backend :: Application
-backend = serve (Proxy @API) server
+backend = serve (Proxy @Backend) server
 
-server = serveDirectoryFileServer
-  "../dist-ghcjs/build/x86_64-linux/ghcjs-8.6.0.1/frontend-0.1.0.0/\
-  \x/frontend/build/frontend/frontend.jsexe"
+server :: Server Backend
+server = update :<|> static
+  where
+    update = pure . (+1)
+    static = serveDirectoryFileServer
+               "../dist-ghcjs/build/x86_64-linux/ghcjs-8.6.0.1/\
+               \frontend-0.1.0.0/x/frontend/build/frontend/frontend.jsexe"
