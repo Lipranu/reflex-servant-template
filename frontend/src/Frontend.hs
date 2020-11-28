507286860b8acbab9b36b11feef8421a9342ca8b
Igor Belousov <lipranu@gmail.com>
2020-11-28 13:01:29 +0700

    Adapted frontend module for common API

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,22 +1,39 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecursiveDo         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
 
 module Frontend (frontend) where
 
 import Common
 import Control.Monad.Fix ( MonadFix )
+import Data.Proxy        ( Proxy (..) )
 import Reflex.Dom
+import Servant.API
+import Servant.Reflex
 
-frontend :: ( DomBuilder t m
-            , MonadHold t m
-            , PostBuild t m
+frontend :: forall m t
+          . ( SupportsServantReflex t m
+            , DomBuilder t m
+            , DomBuilderSpace m ~ GhcjsDomSpace
             , MonadFix m
+            , PostBuild t m
+            , MonadHold t m
             )
          => m ()
 frontend = el "div" $ mdo
-  el "h1" $ text "Current Number: "
-  el "h1" $ display d
-  d <- foldDyn (const (+1)) 0 e
-  e <- button "update"
+  let (get :<|> _) = client (Proxy @API)
+                            (Proxy @m)
+                            (Proxy @())
+                            (constDyn host)
+      host = BaseFullUrl Http "localhost" 8080 ""
+  el "h1" $ text "Get Number: "
+  el "h1" $ do
+    result <- fmapMaybe reqSuccess <$> get trigger
+    number <- holdDyn 0 result
+    display number
+  trigger <- button "Get Number"
   pure ()
 
