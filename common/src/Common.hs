d3328f20e0b28b920dfc519ddca202eed5fb3748
Igor Belousov <lipranu@gmail.com>
2020-11-28 18:44:19 +0700

    Changed common module API, added PingPong type

--- common/src/Common.hs
+++ common/src/Common.hs
@@ -1,9 +1,23 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE TypeOperators      #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DeriveAnyClass     #-}
 
-module Common ( API ) where
+module Common
+  ( API
+  , PingPong (..)
+  ) where
 
-import Servant.API ( Get, JSON, Raw, (:>), (:<|>) )
+import Data.Aeson   ( FromJSON, ToJSON )
+import GHC.Generics ( Generic )
+import Servant.API  ( Post, JSON, Raw, ReqBody, (:>), (:<|>) )
 
-type API = "api" :> Get '[JSON] Integer
+type API = "api" :> ReqBody '[JSON] PingPong :> Post '[JSON] PingPong
   :<|> Raw
+
+data PingPong
+  = Ping
+  | Pong
+  deriving stock (Generic, Show, Eq)
+  deriving anyclass (ToJSON, FromJSON)
