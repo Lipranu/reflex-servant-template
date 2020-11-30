a00316dc8a3a39d063861148a153c8783457db6e
Igor Belousov <lipranu@gmail.com>
2020-11-30 20:27:42 +0700

    Embedded CSS into the frontend app

--- frontend/app/Main.hs
+++ frontend/app/Main.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+
 module Main where
 
 import Frontend
-import Reflex.Dom ( mainWidget )
+import Data.FileEmbed ( embedFile )
+import Reflex.Dom     ( mainWidgetWithCss )
 
 main :: IO ()
-main = mainWidget frontend
+main = mainWidgetWithCss css frontend
+  where css = $(embedFile "../static/style.css")
